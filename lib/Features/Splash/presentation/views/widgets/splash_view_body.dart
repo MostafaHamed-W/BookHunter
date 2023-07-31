251a573c9f56796c1d3ad7a2477849cebd6a4225
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-08-01 00:48:28 +0300

    adding animation to splash view

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,40 @@
+import 'package:book_hunt/Features/Splash/presentation/views/widgets/sliding_logo.dart';
+import 'package:book_hunt/Features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:book_hunt/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with TickerProviderStateMixin {
+  late AnimationController textAnimationController;
+  late Animation<Offset> textSlidingAnimation;
+
+  late AnimationController logoAnimationController;
+  late Animation<Offset> logoAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    textAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    textSlidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 4), end: Offset.zero).animate(textAnimationController);
+    textAnimationController.forward();
+
+    logoAnimationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    logoAnimation = Tween<Offset>(begin: const Offset(-2, 0), end: Offset.zero)
+        .animate(logoAnimationController);
+    logoAnimationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,11 +42,8 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Padding(
-          padding: const EdgeInsets.all(17),
-          child: Image.asset(AssetsData.logo),
-        ),
-        // Center(child: Text("Test Text")),
+        SlidingLogo(logoAnimation: logoAnimation),
+        SlidingText(textSlidingAnimation: textSlidingAnimation),
       ],
     );
   }
