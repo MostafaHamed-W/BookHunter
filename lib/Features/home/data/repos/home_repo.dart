80362604a52f8bc2e793bd954ac697784d7c1fbe
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-08-07 17:31:49 +0300

    create home repo & failure class

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:book_hunt/Features/home/data/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+}
